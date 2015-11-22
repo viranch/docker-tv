dda5aaaeefe60de8c92ab673f44bf84f375c9b0a
Viranch Mehta <viranch.mehta@gmail.com>
2015-11-22 21:55:29 +0530

    Don't enforce basic auth on files for download convenience

--- assets/scripts/start.sh
+++ assets/scripts/start.sh
@@ -3,12 +3,18 @@
 if [[ -n "$AUTH_USER" ]]; then
     echo $AUTH_PASS | htpasswd -ic /etc/apache2/htpasswd $AUTH_USER
     cat << EOF > /etc/apache2/conf-enabled/auth.conf
+# protect directory listing & reverse proxy requests with basic auth
 <Location />
     AuthType Basic
     AuthName "By Invitation Only"
     AuthUserFile /etc/apache2/htpasswd
     Require valid-user
 </Location>
+
+# don't enforce auth on files for download convenience
+<LocationMatch /downloads/.*[^/]$>
+    Require all granted
+</LocationMatch>
 EOF
 fi
 
