ea9d66fe1cd920c322c3c426d8565dcfa4a34689
Viranch Mehta <email@viranch.me>
2017-09-09 17:29:32 +0530

    Use hostname in push notification to indicate source

--- assets/scripts/tr_email.sh
+++ assets/scripts/tr_email.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-test -n "$EMAIL" && echo | mail -S smtp=$(dig +short mx `echo $EMAIL | cut -d'@' -f2` | head -n1 | cut -d' ' -f2) -r tv@home -s "$TR_TORRENT_NAME downloaded" $EMAIL
+test -n "$EMAIL" && echo | mail -S smtp=$(dig +short mx `echo $EMAIL | cut -d'@' -f2` | head -n1 | cut -d' ' -f2) -r tv@`hostname` -s "$TR_TORRENT_NAME downloaded" $EMAIL
