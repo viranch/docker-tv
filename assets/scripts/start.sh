3994c0ddad10fd9043f9baff532a419d2659da72
Viranch Mehta <email@viranch.me>
2019-01-22 01:03:11 -0800

    Remove legacy minidlna stuff

--- assets/scripts/start.sh
+++ assets/scripts/start.sh
@@ -22,8 +22,6 @@ mkdir -p /data/{transmission,watch,downloads}
 test ! -f /data/transmission/settings.json && cp /opt/transmission.json /data/transmission/settings.json
 test ! -L /var/www/html/downloads && ln -s /{data,var/www/html}/downloads
 
-mkdir -p /run/minidlna
-
 list() { l=`echo $@ | sed 's/,/" "/g'`; echo "(\"$l\")"; }
 eval FEEDS=`list $RSS_FEED`
 eval OPTS=`list $TV_OPTS`
