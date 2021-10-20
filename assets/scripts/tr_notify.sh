ca8228012e8ccc35fe229c9bd89d9cb5a788fa82
Viranch Mehta <email@viranch.me>
2021-10-19 20:47:05 -0700

    Specify hostname in pushbullet notifications

--- assets/scripts/tr_notify.sh
+++ assets/scripts/tr_notify.sh
@@ -2,7 +2,7 @@
 
 curl https://api.pushbullet.com/v2/pushes -H 'Content-Type: application/json' -H "Access-Token: $PB_TOKEN" -d @- << EOF
 {
-  "title": "Transmission",
+  "title": "Transmission ($HOSTNAME)",
   "body": "$TR_TORRENT_NAME downloaded",
   "type": "note"
 }
